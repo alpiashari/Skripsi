ab572f59890a750dcf32a9061bf99ab813bb803b
Muhammad Alif <129609799+miezlearning@users.noreply.github.com>
2026-08-05 01:12:49 +0800

    advs

--- tampilan/tampilan/tampilan.py
+++ tampilan/tampilan/tampilan.py
@@ -1,20 +1,25 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-import os
 from pathlib import Path
+
+# ===============================
+# CONFIG & PATH DIRECTORY
+# ===============================
 BASE_DIR = Path(__file__).resolve().parent
 DATASET_DIR = BASE_DIR / "dataset"
-# CONFIG
+IMAGES_DIR = BASE_DIR / "images"
+PREPROCESSING_DIR = BASE_DIR / "Preprocessing"
+PENGUJIAN_DIR = BASE_DIR / "pengujian"
 
 st.set_page_config(
     page_title="KNN Traffic Severity Prediction",
     layout="wide"
 )
 
-
-# LOAD DATASET
-
+# ===============================
+# LOAD DATA & FUNCTION
+# ===============================
 @st.cache_data
 def load_data():
     df_asli = pd.read_csv(DATASET_DIR / "RTA Dataset.csv", sep=";")
@@ -22,42 +27,41 @@ def load_data():
     df_final = pd.read_csv(DATASET_DIR / "Data_final.csv", sep=";")
     return df_asli, df_indonesia, df_final
 
-
 df_asli, df_indonesia, df_final = load_data()
 
 def preprocessing():
-    df_sebelum_null = pd.read_excel("Preprocessing/Sebelum_Null.xlsx", )
-    df_sesudah_null = pd.read_excel("Preprocessing/Sesudah_Null.xlsx")
-    df_transformasi = pd.read_csv("Preprocessing/Transformasi.csv", sep=";")
-
+    df_sebelum_null = pd.read_excel(PREPROCESSING_DIR / "Sebelum_Null.xlsx")
+    df_sesudah_null = pd.read_excel(PREPROCESSING_DIR / "Sesudah_Null.xlsx")
+    df_transformasi = pd.read_csv(PREPROCESSING_DIR / "Transformasi.csv", sep=";")
     return df_sebelum_null, df_sesudah_null, df_transformasi
 
 df_sebelum_null, df_sesudah_null, df_transformasi = preprocessing()
 
 def hasil_pengujian():
-    df_kfold_non_smote = pd.read_excel("pengujian/Kfold_non_smote.xlsx")
-    df_kfold_smote = pd.read_excel("pengujian/Kfold_smote.xlsx")
-    df_k_non_smote = pd.read_excel("pengujian/k_non_smote.xlsx")
-    df_k_smote = pd.read_excel("pengujian/k_smote.xlsx")
-    df_jarak_non_smote = pd.read_excel("pengujian/metric_non_smote.xlsx")
-    df_jarak_smote = pd.read_excel("pengujian/metric_smote.xlsx")
-    df_bobot_non_smote = pd.read_excel("pengujian/weight_non_smote.xlsx")
-    df_bobot_smote = pd.read_excel("pengujian/weight_smote.xlsx")
-
+    df_kfold_non_smote = pd.read_excel(PENGUJIAN_DIR / "Kfold_non_smote.xlsx")
+    df_kfold_smote = pd.read_excel(PENGUJIAN_DIR / "Kfold_smote.xlsx")
+    df_k_non_smote = pd.read_excel(PENGUJIAN_DIR / "k_non_smote.xlsx")
+    df_k_smote = pd.read_excel(PENGUJIAN_DIR / "k_smote.xlsx")
+    df_jarak_non_smote = pd.read_excel(PENGUJIAN_DIR / "metric_non_smote.xlsx")
+    df_jarak_smote = pd.read_excel(PENGUJIAN_DIR / "metric_smote.xlsx")
+    df_bobot_non_smote = pd.read_excel(PENGUJIAN_DIR / "weight_non_smote.xlsx")
+    df_bobot_smote = pd.read_excel(PENGUJIAN_DIR / "weight_smote.xlsx")
     return df_kfold_non_smote, df_kfold_smote, df_k_non_smote, df_k_smote, df_jarak_non_smote, df_jarak_smote, df_bobot_non_smote, df_bobot_smote
 
 df_kfold_non_smote, df_kfold_smote, df_k_non_smote, df_k_smote, df_jarak_non_smote, df_jarak_smote, df_bobot_non_smote, df_bobot_smote = hasil_pengujian()
 
+# ===============================
 # SIDEBAR
-
+# ===============================
 st.sidebar.title("KNN Traffic Severity")
 menu = st.sidebar.radio(
     "Menu",
     ["Dataset", "Preprocessing", "EDA", "Hasil Pengujian", "Evaluasi Model", "Tentang"]
 )
 
+# ===============================
 # HALAMAN DATASET
-
+# ===============================
 if menu == "Dataset":
     st.title("📊 Dataset")
 
@@ -65,69 +69,56 @@ if menu == "Dataset":
 
     with tab1:
         st.dataframe(df_asli, width="stretch")
-
     with tab2:
         st.dataframe(df_indonesia, width="stretch")
-
     with tab3:
         st.dataframe(df_final, width="stretch")
 
-
-# HALAMAN Preprocessing
-
+# ===============================
+# HALAMAN PREPROCESSING
+# ===============================
 elif menu == "Preprocessing":
     st.title("🔄 Preprocessing")
 
     col1, col2 = st.columns(2)
 
-    #Kolom keSatu Penanganan Nilai Null
+    # Kolom keSatu Penanganan Nilai Null
     with col1:
         tab1, tab2 = st.tabs(["Sebelum Null", "Sesudah Null"])
-
         with tab1:
             st.dataframe(df_sebelum_null, width="stretch")
-
         with tab2:
             st.dataframe(df_sesudah_null, width="stretch")
 
     # Kolom keDua Penanganan Outlier
-    tab1, tab2 = st.tabs(["Sebelum Penanganan Outlier", "Sesudah Penanganan Outlier"])
+    with col2:
+        tab1, tab2 = st.tabs(["Sebelum Penanganan Outlier", "Sesudah Penanganan Outlier"])
+        with tab1:
+            st.image(str(IMAGES_DIR / "Sebelum_Penanganan_Outlier.png"), width=630)
+        with tab2:
+            st.image(str(IMAGES_DIR / "Sesudah_Penanganan_Outlier.png"), width=630)
 
-    with tab1:
-        st.image("images/Sebelum_Penanganan_Outlier.png", width=630)
-
-    with tab2:
-        st.image(
-            "images/Sesudah_Penanganan_Outlier.png", width=630)
-
-    #Kolom keTiga Transformasi
+    # Kolom keTiga Transformasi
     col_bawah1, col_bawah2 = st.columns(2)
 
     with col_bawah1:
         tab1, tab2 = st.tabs(["Sebelum Transformasi", "Sesudah Transformasi"])
-
         with tab1:
             st.dataframe(df_asli, width="stretch")
-
         with tab2:
             st.dataframe(df_transformasi, width="stretch")
 
     # Kolom keEmpat Normalisasi
-    col_bawah1, col_bawah2 = st.columns(2)
-
-    with col_bawah1:
+    with col_bawah2:
         tab1, tab2 = st.tabs(["Sebelum Normalisasi", "Sesudah Normalisasi"])
-
         with tab1:
             st.dataframe(df_transformasi, width="stretch")
-
         with tab2:
             st.dataframe(df_final, width="stretch")
 
-    
-    
+# ===============================
 # HALAMAN EDA
-
+# ===============================
 elif menu == "EDA":
     st.title("📈 Exploratory Data Analysis (EDA)")
 
@@ -135,126 +126,94 @@ elif menu == "EDA":
 
     with col1:
         st.subheader("Distribusi Kelas Target")
-        st.image(
-            "images/Distribusi Accident.png",
-            width="stretch"
-        )
+        st.image(str(IMAGES_DIR / "Distribusi Accident.png"), width="stretch")
 
     with col2:
         st.subheader("Distribusi Keseluruhan")
-        st.image(
-            "images/Distribusi Keseluruhan.png",
-            width="stretch"
-        )
+        st.image(str(IMAGES_DIR / "Distribusi Keseluruhan.png"), width="stretch")
 
     # Baris kedua
     col3, col4 = st.columns(2)
 
     with col3:
         st.subheader("Distribusi Korelasi")
-        st.image(
-            "images/Distribusi Korelasi.png",
-            width="stretch"
-        )
+        st.image(str(IMAGES_DIR / "Distribusi Korelasi.png"), width="stretch")
 
     with col4:
         st.subheader("Grafik Batang")
-        st.image(
-            "images/Distribusi_Accident_Bar.png",
-            width="stretch"
-        )
+        st.image(str(IMAGES_DIR / "Distribusi_Accident_Bar.png"), width="stretch")
 
-
-# Halaman Hasil Pengujian
-
+# ===============================
+# HALAMAN HASIL PENGUJIAN
+# ===============================
 elif menu == "Hasil Pengujian":
     st.title("📊 Hasil Pengujian")
 
     col1, col2 = st.columns(2)
 
-    #Kolom keSatu Pengujian
+    # Kolom keSatu Pengujian
     with col1:
-        tab1, tab2 = st.tabs(["Kfold Non Smote", "Kfoold Smote"])
-
+        tab1, tab2 = st.tabs(["Kfold Non Smote", "Kfold Smote"])
         with tab1:
             st.dataframe(df_kfold_non_smote, width="stretch")
             st.caption("Best Kfold Non Smote : 3")
-
         with tab2:
             st.dataframe(df_kfold_smote, width="stretch")
             st.caption("Best Kfold Smote : 5")
 
-    #Kolom keDua Pengujian
-    col_bawah1, col_bawah2 = st.columns(2)
-
-    with col_bawah1:
+    # Kolom keDua Pengujian
+    with col2:
         tab1, tab2 = st.tabs(["K Non Smote", "K Smote"])
-
         with tab1:
             st.dataframe(df_k_non_smote, width="stretch")
             st.caption("Best K Non Smote : 5")
-
         with tab2:
             st.dataframe(df_k_smote, width="stretch")
             st.caption("Best K Smote : 3")
 
-    #Kolom keTiga Pengujian
+    # Kolom keTiga Pengujian
     col_bawah1, col_bawah2 = st.columns(2)
 
     with col_bawah1:
         tab1, tab2 = st.tabs(["Metric Non Smote", "Metric Smote"])
-
         with tab1:
             st.dataframe(df_jarak_non_smote, width="stretch")
             st.caption("Best Metric Non Smote : Euclidean")
-
         with tab2:
             st.dataframe(df_jarak_smote, width="stretch")
             st.caption("Best Metric Smote : Manhattan")
 
-        #Kolom keEmpat Pengujian
-    col_bawah1, col_bawah2 = st.columns(2)
-
-    with col_bawah1:
+    # Kolom keEmpat Pengujian
+    with col_bawah2:
         tab1, tab2 = st.tabs(["Weight Non Smote", "Weight Smote"])
-
         with tab1:
             st.dataframe(df_bobot_non_smote, width="stretch")
             st.caption("Best Weight Non Smote : Uniform")
-
         with tab2:
             st.dataframe(df_bobot_smote, width="stretch")
             st.caption("Best Weight Smote : Distance")
 
-
+# ===============================
 # HALAMAN EVALUASI MODEL
-
+# ===============================
 elif menu == "Evaluasi Model":
     st.title("📊 Evaluasi Model")
 
-
     # CONFUSION MATRIX
-
     st.subheader("Confusion Matrix")
 
     col1, col2 = st.columns(2)
 
     with col1:
-        base_dir = os.path.dirname(__file__)
-        img_path_dummy = os.path.join(base_dir, "images", "Confusion_matrix_dummy.png")
-        img_path_knn_manual_non_smote = os.path.join(base_dir, "images", "Confusion_matrix_knn_manual_non_smote.png")
-        img_path_knn_manual_smote = os.path.join(base_dir, "images", "Confusion_matrix_knn_manual_smote.png")
-        st.image(img_path_dummy, caption="Dummy Classifier", width="stretch")
-        st.image(img_path_knn_manual_non_smote, caption="Confusion Matrix KNN Manual (Non SMOTE)", width="stretch")
-        st.image(img_path_knn_manual_smote, caption="Confusion Matrix KNN Manual (SMOTE)", width="stretch")
+        st.image(str(IMAGES_DIR / "Confusion_matrix_dummy.png"), caption="Dummy Classifier", width="stretch")
+        st.image(str(IMAGES_DIR / "Confusion_matrix_knn_manual_non_smote.png"), caption="Confusion Matrix KNN Manual (Non SMOTE)", width="stretch")
+        st.image(str(IMAGES_DIR / "Confusion_matrix_knn_manual_smote.png"), caption="Confusion Matrix KNN Manual (SMOTE)", width="stretch")
 
     with col2:
-        st.image("images/Confusion_matrix_grid_non_smote.png", caption="Confusion Matrix Grid Search CV (Non SMOTE)", width="stretch")
-        st.image("images/Confusion_matrix_grid_smote.png", caption="Confusion Matrix Grid Search CV (SMOTE)", width="stretch")
-
+        st.image(str(IMAGES_DIR / "Confusion_matrix_grid_non_smote.png"), caption="Confusion Matrix Grid Search CV (Non SMOTE)", width="stretch")
+        st.image(str(IMAGES_DIR / "Confusion_matrix_grid_smote.png"), caption="Confusion Matrix Grid Search CV (SMOTE)", width="stretch")
 
     # CLASSIFICATION REPORT
-
     st.subheader("Dummy Classifier")
 
     data_knn_manual_non_smote = {
@@ -268,12 +227,10 @@ elif menu == "Evaluasi Model":
     st.dataframe(df_knn_manual_non_smote_report, width="stretch")
     
     accuracy = 0.84686
-
     st.subheader("Accuracy")
     st.metric(label="Akurasi Model", value=f"{accuracy*100:.2f}%")
 
     st.subheader("Classification Report KNN Manual (Non SMOTE)")
-
     data_knn_manual_non_smote = {
         "Kelas": ["(0) Slight Injury", "(1) Serious Injury", "(2) Fatal Injury"],
         "Precision": [0.25000, 0.16667, 0.84747],
@@ -285,13 +242,10 @@ elif menu == "Evaluasi Model":
     st.dataframe(df_knn_manual_non_smote_report, width="stretch")
     
     accuracy = 0.83180
-
     st.subheader("Accuracy")
     st.metric(label="Akurasi Model", value=f"{accuracy*100:.2f}%")
     
-    
     st.subheader("Classification Report KNN Manual (SMOTE)")
-
     data_knn_manual_smote = {
         "Kelas": ["(0) Slight Injury", "(1) Serious Injury", "(2) Fatal Injury"],
         "Precision": [0.07895, 0.13169, 0.84573],
@@ -303,12 +257,10 @@ elif menu == "Evaluasi Model":
     st.dataframe(df_knn_manual_smote_report, width="stretch")
     
     accuracy = 0.67615
-
     st.subheader("Accuracy")
     st.metric(label="Akurasi Model", value=f"{accuracy*100:.2f}%")
     
     st.subheader("Classification Report KNN Grid Search CV (Non SMOTE)")
-
     data_grid_non_smote = {
         "Kelas": ["(0) Slight Injury", "(1) Serious Injury", "(2) Fatal Injury"],
         "Precision": [0.27778, 0.25000, 0.85426],
@@ -320,12 +272,10 @@ elif menu == "Evaluasi Model":
     st.dataframe(df_grid_non_smote, width="stretch")
     
     accuracy = 0.82190
-
     st.subheader("Accuracy")
     st.metric(label="Akurasi Model", value=f"{accuracy*100:.2f}%")
     
     st.subheader("Classification Report KNN Grid Search CV (SMOTE)")
-    
     data_grid_smote= {
         "Kelas": ["(0) Slight Injury", "(1) Serious Injury", "(2) Fatal Injury"],
         "Precision": [0.10526, 0.17255, 0.86079],
@@ -337,15 +287,12 @@ elif menu == "Evaluasi Model":
     st.dataframe(df_grid_smote, width="stretch")
     
     accuracy = 0.68983
-
     st.subheader("Accuracy")
     st.metric(label="Akurasi Model", value=f"{accuracy*100:.2f}%")
-
 
     # ===========================
     # PERBANDINGAN
     # ===========================
-
     st.subheader("Perbandingan Performa Seluruh Model")
     
     data_seluruh_model= {
@@ -357,7 +304,6 @@ elif menu == "Evaluasi Model":
     }
 
     df_seluruh_model = pd.DataFrame(data_seluruh_model)
-    # st.dataframe(df_seluruh_model, width="stretch")
 
     st.dataframe(
     df_seluruh_model.style.format({
@@ -365,11 +311,10 @@ elif menu == "Evaluasi Model":
         "Precision": "{:.2%}",
         "Recall": "{:.2%}",
         "F1-Score": "{:.2%}"
-    }),width="stretch")
+    }), width="stretch")
 
     st.subheader("Perbandingan DUMMY, SMOTE, dan Non-SMOTE")
-
-    st.image("images/perbandingan_hasil_semua_model.png", width="stretch")
+    st.image(str(IMAGES_DIR / "perbandingan_hasil_semua_model.png"), width="stretch")
 
 # ===============================
 # HALAMAN TENTANG
@@ -383,5 +328,4 @@ else:
     Dataset diambil dari Kaggle:
     Road Traffic Severity Classification.
     """)
-
     # python -m streamlit run tampilan\tampilan.py
